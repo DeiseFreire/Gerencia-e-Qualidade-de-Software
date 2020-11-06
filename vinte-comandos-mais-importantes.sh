28e1e89ecdd2ce791f3db9c5d1aeb3287d126d64
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-05 22:05:29 -0300

    Create vinte-comandos-mais-importantes.sh

---
+++ vinte-comandos-mais-importantes.sh
@@ -0,0 +1,23 @@
+# ---------------------------------------------------------------------------------------------
+# 20 comandos mais importantes para iniciantes no Linux - 11 a 20
+# https://www.youtube.com/watch?v=1HapBf0cnmU&list=PLucm8g_ezqNp92MmkF9p_cj4yhT-fCTl7&index=156
+# ---------------------------------------------------------------------------------------------
+
+# arq.teste é um arquivo de texto para visualizar o 
+# conteúdo dele direto no terminal usando um cat 
+deise@debian:~$ cat arq.teste # [ENTER] ele mostra o 
+# conteúdo do arquivo para mim sem que eu tenha 
+# necessariamente que abrir esse arquivo para edição
+# O comando é muito importante 
+deise@debian:~$ sudo # ele permite que você execute tarefas 
+# administrativas, ou seja, tarefas como o usuário 
+# administrador do sistema sem no entando ser 
+# administrador do sistema e sem precisar logar como 
+# administrador do sistema então sudo é muito importante 
+# O exemplo clássico é isso aqui vamos supor que eu 
+# queira verificar o IP da minha máquina. Existe um 
+# comando ifconfig que mostra o IP da minha máquina 
+deise@debian: ~$ ifconfig # se eu tentar executar esse comando 
+deise@debian: ~$ ifconfig
+bash: ifconfig: comando não encontrado 
+fabio@debian: ~$ 
