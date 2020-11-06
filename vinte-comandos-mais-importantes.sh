ca3ccac4f3a7180032ecadf126ebcbfd4b5b96f8
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-06 14:44:38 -0300

    Update vinte-comandos-mais-importantes.sh

--- vinte-comandos-mais-importantes.sh
+++ vinte-comandos-mais-importantes.sh
@@ -1,7 +1,6 @@
-# arq.teste é um arquivo de texto para visualizar o 
-# conteúdo dele direto no terminal usando um cat 
-deise@debian:~$ cat arq.teste # ele mostra o 
-# conteúdo do arquivo para mim sem que eu tenha
+# arq.teste é um arquivo de texto para visualizar o conteúdo dele direto no terminal 
+# Usando um cat ele mostra o conteúdo do arquivo para mim. Sem que eu tenha 
+deise@debian:~$ cat arq.teste 
 # necessariamente que abrir esse arquivo para edição
 deise@debian:~$ sudo # ele permite que você execute tarefas 
 # administrativas, ou seja, tarefas como o usuário 
@@ -11,8 +10,11 @@ deise@debian:~$ sudo # ele permite que você execute tarefas
 # O exemplo clássico é isso aqui vamos supor que eu 
 # queira verificar o IP da minha máquina. Existe um 
 # comando ifconfig que mostra o IP da minha máquina 
-deise@debian: ~$ ifconfig # se eu tentar executar esse comando 
+deise@debian: ~$ ifconfig # se eu tentar executar esse  como usuário normal, usuário do dia-a-dia
 deise@debian: ~$ ifconfig
-bash: ifconfig: comando não encontrado 
+bash: ifconfig: comando não encontrado # ele me retorna essa mensagem 
 deise@debian: ~$ 
-
+# Na verdade o comando existe o que não existe é a permissão para eu executar esse 
+# comando diretamente então eu posso usar o sudo seguido do comando que eu quero 
+# executar deise@debian: ~$ sudo ifconfig [ENTER] e aí ele vai executar o comando com 
+# privilégio administrativo e vai trazer a informação que eu preciso. 
