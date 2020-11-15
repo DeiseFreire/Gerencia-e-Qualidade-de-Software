1a04b30040210b2a9d3b74a10c9f982e707d7977
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-15 17:13:22 -0300

    Update vinte-comandos-mais-importantes.sh

--- vinte-comandos-mais-importantes.sh
+++ vinte-comandos-mais-importantes.sh
@@ -2,23 +2,17 @@
 # Usando um cat ele mostra o conteúdo do arquivo para mim. Sem que eu tenha 
 deise@debian:~$ cat arq.teste 
 # necessariamente que abrir esse arquivo para edição
-deise@debian:~$ sudo # ele permite que você execute tarefas 
-# administrativas, ou seja, tarefas como o usuário 
-# administrador do sistema sem no entando ser 
-# administrador do sistema e sem precisar logar como 
-# administrador do sistema então sudo é muito importante 
-# O exemplo clássico é isso aqui vamos supor que eu 
-# queira verificar o IP da minha máquina. Existe um 
-# comando ifconfig que mostra o IP da minha máquina 
+deise@debian:~$ sudo # ele permite que você execute tarefas administrativas, como o usuário administrador do sistema sem no entando ser 
+# administrador do sistema e sem precisar logar como administrador do sistema 
+# Existe um comando ifconfig que mostra o IP da máquina 
 deise@debian: ~$ ifconfig # se eu tentar executar esse  como usuário normal, usuário do dia-a-dia
-deise@debian: ~$ ifconfig
-bash: ifconfig: comando não encontrado # ele me retorna essa mensagem 
+bash: ifconfig: comando não encontrado # ele me retorna esta mensagem 
 deise@debian: ~$ 
 # Na verdade o comando existe o que não existe é a permissão para eu executar esse 
 # comando diretamente então eu posso usar o sudo seguido do comando que eu quero 
 executar deise@debian: ~$ sudo ifconfig # [ENTER] e aí ele vai executar o comando com 
 # privilégio administrativo e vai trazer a informação que eu preciso.  
 deise@debian: ~$ dnf # O fedora utiliza o dnf que é similar ao apt-get
-deise@debian: ~$ sudo apt-get update # faz o processo de ir na internet, nos repositórios configurados no sistema linux, e baixa os pacotes necessários 
-deise@debian: ~$ sudo apt-get install tree # vai na internet baixa o pacote e faz a instalação 
+deise@debian: ~$ sudo apt-get update # faz o processo de ir na internet, nos repositórios do sistema linux, e baixa os pacotes necessários 
+deise@debian: ~$ sudo apt-get install tree # vai na internet, baixa o pacote e faz a instalação 
  
